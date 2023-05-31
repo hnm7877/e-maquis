c31890b1623f99c491b387f92c895174c0b77632
starkspartacus <69165249+starkspartacus@users.noreply.github.com>
2023-05-31 02:34:02 +0200

    init bilan responsive

--- src/assets/js/cardDetail.js
+++ src/assets/js/cardDetail.js
@@ -2,6 +2,7 @@
 
 import '../../assets/lib/jquery/jquery.js';
 import '../../assets/lib/popper/popper.js';
+import '../../assets/lib/bootstrap/bootstrap.bundle.min.js';
 import 'bootstrap/dist/js/bootstrap.js';
 import '../../assets/lib/perfect-scrollbar/js/perfect-scrollbar.jquery.js';
 // import '../../assets/lib/moment/moment.js';
@@ -9,11 +10,11 @@ import '../../assets/lib/jquery-ui/jquery-ui.js';
 import '../../assets/lib/jquery-switchbutton/jquery.switchButton.js';
 import '../../assets/lib/peity/jquery.peity.js';
 import '../../assets/lib/jquery.sparkline.bower/jquery.sparkline.min.js';
-import '../../assets/lib/d3/d3.js';
-import '../../assets/lib/rickshaw/rickshaw.min.js';
+// import '../../assets/lib/d3/d3.js';
+// import '../../assets/lib/rickshaw/rickshaw.min.js';
 import '../../assets/js/ResizeSensor.js';
-import '../../assets/js/bracket.js';
-import '../../assets/js/widgets.js';
+// import '../../assets/js/bracket.js';
+// import '../../assets/js/widgets.js';
 $(function(){
     'use strict'
 
--- src/components/CardBilan/index.tsx
+++ src/components/CardBilan/index.tsx
@@ -2,14 +2,13 @@
 
 export const CardBilan = () => {
     return (
-        <div className="row row-sm col-sm-12 col-md-12 col-xl-12">
-            <div className="col-sm-6 col-xl-3">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-xl-4">
+            <div className="col">
                 <div className="bg-teal rounded overflow-hidden">
                     <div className="pd-25 d-flex align-items-center">
                         <i className="ion ion-earth tx-60 lh-0 tx-white op-7"></i>
                         <div className="mg-l-20">
-                            <p className="tx-10 tx-spacing-1 tx-mont tx-medium tx-uppercase tx-white-8 mg-b-10">Today's
-                                Visits</p>
+                            <p className="tx-10 tx-spacing-1 tx-mont tx-medium tx-uppercase tx-white-8 mg-b-10">Today's Visits</p>
                             <p className="tx-24 tx-white tx-lato tx-bold mg-b-2 lh-1">1,975,224</p>
                             <span className="tx-11 tx-roboto tx-white-6">24% higher yesterday</span>
                         </div>
@@ -17,13 +16,12 @@ export const CardBilan = () => {
                 </div>
             </div>
 
-            <div className="col-sm-6 col-xl-3 mg-t-20 mg-sm-t-0">
+            <div className="col">
                 <div className="bg-danger rounded overflow-hidden">
                     <div className="pd-25 d-flex align-items-center">
                         <i className="ion ion-bag tx-60 lh-0 tx-white op-7"></i>
                         <div className="mg-l-20">
-                            <p className="tx-10 tx-spacing-1 tx-mont tx-medium tx-uppercase tx-white-8 mg-b-10">Today
-                                Sales</p>
+                            <p className="tx-10 tx-spacing-1 tx-mont tx-medium tx-uppercase tx-white-8 mg-b-10">Today Sales</p>
                             <p className="tx-24 tx-white tx-lato tx-bold mg-b-2 lh-1">$329,291</p>
                             <span className="tx-11 tx-roboto tx-white-6">$390,212 before tax</span>
                         </div>
@@ -31,13 +29,12 @@ export const CardBilan = () => {
                 </div>
             </div>
 
-            <div className="col-sm-6 col-xl-3 mg-t-20 mg-xl-t-0">
+            <div className="col">
                 <div className="bg-primary rounded overflow-hidden">
                     <div className="pd-25 d-flex align-items-center">
                         <i className="ion ion-monitor tx-60 lh-0 tx-white op-7"></i>
                         <div className="mg-l-20">
-                            <p className="tx-10 tx-spacing-1 tx-mont tx-medium tx-uppercase tx-white-8 mg-b-10">%
-                                Unique Visits</p>
+                            <p className="tx-10 tx-spacing-1 tx-mont tx-medium tx-uppercase tx-white-8 mg-b-10">% Unique Visits</p>
                             <p className="tx-24 tx-white tx-lato tx-bold mg-b-2 lh-1">54.45%</p>
                             <span className="tx-11 tx-roboto tx-white-6">23% average duration</span>
                         </div>
@@ -45,13 +42,12 @@ export const CardBilan = () => {
                 </div>
             </div>
 
-            <div className="col-sm-6 col-xl-3 mg-t-20 mg-xl-t-0">
+            <div className="col">
                 <div className="bg-br-primary rounded overflow-hidden">
                     <div className="pd-25 d-flex align-items-center">
                         <i className="ion ion-clock tx-60 lh-0 tx-white op-7"></i>
                         <div className="mg-l-20">
-                            <p className="tx-10 tx-spacing-1 tx-mont tx-medium tx-uppercase tx-white-8 mg-b-10">Bounce
-                                Rate</p>
+                            <p className="tx-10 tx-spacing-1 tx-mont tx-medium tx-uppercase tx-white-8 mg-b-10">Bounce Rate</p>
                             <p className="tx-24 tx-white tx-lato tx-bold mg-b-2 lh-1">32.16%</p>
                             <span className="tx-11 tx-roboto tx-white-6">65.45% on average time</span>
                         </div>
@@ -59,6 +55,7 @@ export const CardBilan = () => {
                 </div>
             </div>
         </div>
+
     )
 }
 

8acdb308f1cd8099e62e55eaee4472001141fe4f
starkspartacus <69165249+starkspartacus@users.noreply.github.com>
2023-05-31 03:53:51 +0200

    Update index.tsx

--- src/components/TableBilan/index.tsx
+++ src/components/TableBilan/index.tsx
@@ -31,7 +31,7 @@ export const TableBilan = () => {
         table-hover
         table-responsive'
       bordered={true}
-    >
+     >
       <thead>
         <tr className='table-dark en_tete'>
           <th>Catégories</th>
@@ -91,48 +91,4 @@ export const TableBilan = () => {
     </Table>
   );
 };
-    return (
-        <div className="table-responsive">
-            <Table className="table-striped table-bordered table-hover" bordered={true}>
-                <thead>
-                <tr className="table-dark en_tete">
-                    <th>Catégories</th>
-                    <th>Produits</th>
-                    <th>Quantités</th>
-                    <th>Taille</th>
-                    <th>Prix d'achat</th>
-                    <th>Vente totale</th>
-                    <th>Bénéfice</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Soda</td>
-                    <td>coca</td>
-                    <td>200</td>
-                    <td>33 cl</td>
-                    <td>150 FCFA</td>
-                    <td>500 FCFA</td>
-                    <td>3000 FCFA</td>
-                </tr>
-                <tr>
-                    <td>Soda</td>
-                    <td>coca</td>
-                    <td>200</td>
-                    <td>33 cl</td>
-                    <td>150 FCFA</td>
-                    <td>500 FCFA</td>
-                    <td>3000 FCFA</td>
-                </tr>
-                <tr>
-                    <td colSpan={4} />
-                    <td>Total Prix d'achat:  FCFA</td>
-                    <td>Total Vente totale:  FCFA</td>
-                    <td>Total Bénéfice:  FCFA</td>
-                </tr>
-                </tbody>
-            </Table>
-        </div>
 
-    )
-}
